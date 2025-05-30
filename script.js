9deeea30e19a066678eca52312836ad06c0bdde8
derya <dheriau@gmail.com>
2025-05-30 13:24:02 +0300

    proje

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('contactBtn').addEventListener('click', function() {
+    window.location.href = "mailto:dheriau@gmail.com?subject=pettipawz.com%20alan%20adı%20satışı&body=Merhaba,%20pettipawz.com%20alan%20adıyla%20ilgileniyorum.";
+});
